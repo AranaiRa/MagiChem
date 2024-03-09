8e66ca790058d25ba7896f23b3f529ef80de1cfb
AranaiRa <aranaiseraph@gmail.com>
2024-03-09 05:09:41 -0500

    Break particles on water router (but only if the player breaks the top half >:/)

--- src/main/java/com/aranaira/magichem/block/ActuatorFireRouterBlock.java
+++ src/main/java/com/aranaira/magichem/block/ActuatorFireRouterBlock.java
@@ -4,6 +4,8 @@ import com.aranaira.magichem.block.entity.routers.ActuatorFireRouterBlockEntity;
 import com.aranaira.magichem.block.entity.routers.ActuatorWaterRouterBlockEntity;
 import com.mna.items.base.INoCreativeTab;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
@@ -24,5 +26,14 @@ public class ActuatorFireRouterBlock extends BaseActuatorRouterBlock implements 
     @Override
     public RenderShape getRenderShape(BlockState pState) {
         return RenderShape.INVISIBLE;
+    }
+
+    @Override
+    protected void spawnDestroyParticles(Level pLevel, Player pPlayer, BlockPos pPos, BlockState pState) {
+        BlockEntity be = pLevel.getBlockEntity(pPos);
+        if(be instanceof ActuatorFireRouterBlockEntity router) {
+            BlockState masterState = router.getMaster().getBlockState();
+            pLevel.levelEvent(pPlayer, 2001, pPos, getId(masterState));
+        }
     }
 }
