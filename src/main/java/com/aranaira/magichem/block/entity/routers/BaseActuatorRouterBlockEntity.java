abe9429157531189f3617f7aedb8201614d3eb6e
AranaiRa <aranaiseraph@gmail.com>
2024-04-20 06:59:53 -0400

    Hopefully prevent routers from having null master positions

--- src/main/java/com/aranaira/magichem/block/entity/routers/BaseActuatorRouterBlockEntity.java
+++ src/main/java/com/aranaira/magichem/block/entity/routers/BaseActuatorRouterBlockEntity.java
@@ -53,6 +53,8 @@ public class BaseActuatorRouterBlockEntity extends BlockEntity implements MenuPr
 
     @Override
     public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+        if(getMasterPos() == null)
+            return LazyOptional.empty();
         return getMaster().getCapability(cap, side);
     }
 
