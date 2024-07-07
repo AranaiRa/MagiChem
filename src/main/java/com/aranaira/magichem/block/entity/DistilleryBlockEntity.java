d473e8bbceec3f34f95a0fad13fac4c8f196252f
AranaiRa <aranaiseraph@gmail.com>
2024-07-07 16:29:01 -0400

    Distillery fuel no longer turns into a bucket

--- src/main/java/com/aranaira/magichem/block/entity/DistilleryBlockEntity.java
+++ src/main/java/com/aranaira/magichem/block/entity/DistilleryBlockEntity.java
@@ -371,7 +371,7 @@ public class DistilleryBlockEntity extends AbstractDistillationBlockEntity imple
                     burnTime = mi.get();
                 } else if(fuelStack.getItem() == ItemInit.FLUID_JUG_INFINITE_LAVA.get()) {
                     burnTime = ForgeHooks.getBurnTime(new ItemStack(Items.LAVA_BUCKET), RecipeType.SMELTING);
-                } else if(fuelStack.getItem() != Items.LAVA_BUCKET) {
+                } else if(fuelStack.getItem() == Items.LAVA_BUCKET) {
                     fuelStack = new ItemStack(Items.BUCKET);
                 } else {
                     fuelStack.shrink(1);
