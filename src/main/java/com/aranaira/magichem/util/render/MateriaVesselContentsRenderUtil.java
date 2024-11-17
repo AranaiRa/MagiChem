9ffece2d962792ac038d2b9483c1284cf996b187
AranaiRa <aranaiseraph@gmail.com>
2024-11-17 02:47:01 -0500

    GCoF hovering materia vessels should no longer have texture errors

--- src/main/java/com/aranaira/magichem/util/render/MateriaVesselContentsRenderUtil.java
+++ src/main/java/com/aranaira/magichem/util/render/MateriaVesselContentsRenderUtil.java
@@ -40,20 +40,30 @@ public class MateriaVesselContentsRenderUtil {
         float height = FLOATING_VESSEL_FLUID_HEIGHT_MAX * fillAmount;
         TextureAtlasSprite texture = Minecraft.getInstance().getTextureAtlas(InventoryMenu.BLOCK_ATLAS).apply(FLUID_TEXTURE);
 
-        RenderUtils.renderFace(Direction.UP, pose, normal, consumer, texture,
-                FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_START_Y +height, FLOATING_VESSEL_FLUID_WIDTH, FLOATING_VESSEL_FLUID_WIDTH, color, packedLight);
+        RenderUtils.renderFaceWithUV(Direction.UP, pose, normal, consumer, texture,
+                FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_START_Y +height, FLOATING_VESSEL_FLUID_WIDTH, FLOATING_VESSEL_FLUID_WIDTH,
+                0.75f, 0.875f, 0.75f, 0.875f,
+                color, packedLight);
 
-        RenderUtils.renderFace(Direction.NORTH, pose, normal, consumer, texture,
-                FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_START_Y, FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_WIDTH, height, color, packedLight);
+        RenderUtils.renderFaceWithUV(Direction.NORTH, pose, normal, consumer, texture,
+                FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_START_Y, FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_WIDTH, height,
+                0, 0.125f, 0.0625f, 0.0625f + height,
+                color, packedLight);
 
-        RenderUtils.renderFace(Direction.EAST, pose, normal, consumer, texture,
-                FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_START_Y, FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_WIDTH, height, color, packedLight);
+        RenderUtils.renderFaceWithUV(Direction.EAST, pose, normal, consumer, texture,
+                FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_START_Y, FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_WIDTH, height,
+                0, 0.125f, 0.0625f, 0.0625f + height,
+                color, packedLight);
 
-        RenderUtils.renderFace(Direction.SOUTH, pose, normal, consumer, texture,
-                FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_START_Y, 1 + FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_WIDTH, height, color, packedLight);
+        RenderUtils.renderFaceWithUV(Direction.SOUTH, pose, normal, consumer, texture,
+                FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_START_Y, 1 + FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_WIDTH, height,
+                0, 0.125f, 0.0625f, 0.0625f + height,
+                color, packedLight);
 
-        RenderUtils.renderFace(Direction.WEST, pose, normal, consumer, texture,
-                FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_START_Y, 1 + FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_WIDTH, height, color, packedLight);
+        RenderUtils.renderFaceWithUV(Direction.WEST, pose, normal, consumer, texture,
+                FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_START_Y, 1 + FLOATING_VESSEL_FLUID_START_XZ, FLOATING_VESSEL_FLUID_WIDTH, height,
+                0, 0.125f, 0.0625f, 0.0625f + height,
+                color, packedLight);
     }
 
     public static void renderJarFluidContents(Matrix4f pose, Matrix3f normal, VertexConsumer consumer, float fillAmount, int color, int packedLight) {
