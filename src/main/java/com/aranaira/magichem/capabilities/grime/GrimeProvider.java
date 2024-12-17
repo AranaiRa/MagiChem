c1eb872ff6ae27a7e049e5ad4a81d3f27d9cacae
AranaiRa <aranaiseraph@gmail.com>
2024-12-17 13:32:42 -0500

    Grime should no longer go negative on servers

--- src/main/java/com/aranaira/magichem/capabilities/grime/GrimeProvider.java
+++ src/main/java/com/aranaira/magichem/capabilities/grime/GrimeProvider.java
@@ -29,7 +29,8 @@ public class GrimeProvider implements ICapabilitySerializable<Tag> {
     public Tag serializeNBT() {
         IGrimeCapability instance = this.holder.orElse(new GrimeCapability());
         CompoundTag nbt = new CompoundTag();
-        nbt.putInt("grime", instance.getGrime());
+        nbt.putInt("grime", instance.getGrime() & 0xffff);
+        nbt.putInt("grime2", (instance.getGrime() >> 16) & 0xffff);
         return nbt;
     }
 
@@ -37,7 +38,12 @@ public class GrimeProvider implements ICapabilitySerializable<Tag> {
     public void deserializeNBT(Tag nbt) {
         IGrimeCapability instance = this.holder.orElse(new GrimeCapability());
         if(nbt instanceof CompoundTag ct) {
-            if(ct.contains("grime")) {
+            if(ct.contains("grime2")) {
+                int grime = ct.getInt("grime");
+                int grime2 = ct.getInt("grime2") << 16;
+                instance.setGrime(grime + grime2);
+            }
+            else if(ct.contains("grime")) {
                 int grime = ct.getInt("grime");
                 instance.setGrime(grime);
             }
