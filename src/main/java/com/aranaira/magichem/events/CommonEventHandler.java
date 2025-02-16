3b9c084945917a0c32a40db76b47df45baea7731
AranaiRa <aranaiseraph@gmail.com>
2025-02-16 16:27:37 -0500

    Grand Fusery has a ports readout

--- src/main/java/com/aranaira/magichem/events/CommonEventHandler.java
+++ src/main/java/com/aranaira/magichem/events/CommonEventHandler.java
@@ -414,17 +414,17 @@ public class CommonEventHandler {
                         }
                     } else if (blockEntity instanceof GrandFuseryRouterBlockEntity gfrbe) {
                         boolean hasLaboratoryUpgrade = state.getValue(HAS_LABORATORY_UPGRADE);
-                        GrandCentrifugeRouterType routerType = GrandCentrifugeRouterBlock.unmapRouterTypeFromInt(state.getValue(ROUTER_TYPE_GRAND_CENTRIFUGE));
+                        GrandFuseryRouterType routerType = GrandFuseryRouterBlock.unmapRouterTypeFromInt(state.getValue(ROUTER_TYPE_GRAND_FUSERY));
 
-                        if(routerType == GrandCentrifugeRouterType.PLUG_BACK_LEFT ||
-                           routerType == GrandCentrifugeRouterType.PLUG_BACK_RIGHT ||
-                           routerType == GrandCentrifugeRouterType.PLUG_FRONT_LEFT ||
-                           routerType == GrandCentrifugeRouterType.PLUG_FRONT_RIGHT) {
+                        if(routerType == GrandFuseryRouterType.PLUG_BACK_LEFT ||
+                           routerType == GrandFuseryRouterType.PLUG_BACK_RIGHT ||
+                           routerType == GrandFuseryRouterType.PLUG_FRONT_LEFT ||
+                           routerType == GrandFuseryRouterType.PLUG_FRONT_RIGHT) {
 
                             if(CommonEventHelper.checkDirectionAndPos(gfrbe.getPlugDirection(), bhr)) {
                                 mode = 1;
                             }
-                        } else if(hasLaboratoryUpgrade && (routerType == GrandCentrifugeRouterType.PLUG_MID_LEFT || routerType == GrandCentrifugeRouterType.PLUG_MID_RIGHT)) {
+                        } else if(hasLaboratoryUpgrade && (routerType == GrandFuseryRouterType.PLUG_MID_LEFT || routerType == GrandFuseryRouterType.PLUG_MID_RIGHT)) {
 
                             if(CommonEventHelper.checkDirectionAndPos(gfrbe.getPlugDirection(), bhr)) {
                                 mode = 1;
