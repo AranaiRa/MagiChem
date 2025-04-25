fecebfc58ce8d68174c5eb92c5df79930f7191de
AranaiRa <aranaiseraph@gmail.com>
2025-04-25 04:53:58 -0400

    New distillation/fabrication required parameters are functional and regex'd

---
+++ src/main/java/com/aranaira/magichem/foundation/enums/DistillationSourceCategory.java
@@ -0,0 +1,10 @@
+package com.aranaira.magichem.foundation.enums;
+
+public enum DistillationSourceCategory {
+    CRAFTABLE,
+    GATHERABLE,
+    FARMABLE,
+    RENEWABLE,
+    TROPHY,
+    RARE
+}
--- src/main/java/com/aranaira/magichem/recipe/DistillationFabricationRecipe.java
+++ src/main/java/com/aranaira/magichem/recipe/DistillationFabricationRecipe.java
@@ -1,6 +1,7 @@
 package com.aranaira.magichem.recipe;
 
 import com.aranaira.magichem.MagiChemMod;
+import com.aranaira.magichem.foundation.enums.DistillationSourceCategory;
 import com.aranaira.magichem.item.MateriaItem;
 import com.aranaira.magichem.registry.ItemRegistry;
 import com.google.gson.JsonArray;
@@ -29,16 +30,22 @@ public class DistillationFabricationRecipe implements Recipe<SimpleContainer> {
     private final ResourceLocation id;
     private final ItemStack alchemyObject;
     private final NonNullList<ItemStack> componentMateria;
-    private final boolean distillOnly;
+    private final byte wisdom, categories, batchSize;
     private final float outputRate;
+    private final ResourceLocation requiredAdvancement, forbiddenAdvancement;
 
-    public DistillationFabricationRecipe(ResourceLocation id, ItemStack alchemyObject,
-                                         NonNullList<ItemStack> componentMateria, boolean distillOnly, float outputRate) {
-        this.id = id;
-        this.alchemyObject = alchemyObject;
-        this.componentMateria = componentMateria;
-        this.distillOnly = distillOnly;
-        this.outputRate = outputRate;
+    public DistillationFabricationRecipe(ResourceLocation pID, ItemStack pAlchemyObject,
+                                         NonNullList<ItemStack> pComponentMateria, byte pWisdom, byte pCategories, byte pBatchSize, float pOutputRate,
+                                         ResourceLocation pRequiredAdvancement, ResourceLocation pForbiddenAdvancement) {
+        this.id = pID;
+        this.alchemyObject = pAlchemyObject;
+        this.componentMateria = pComponentMateria;
+        this.wisdom = pWisdom;
+        this.categories = pCategories;
+        this.outputRate = pOutputRate;
+        this.batchSize = pBatchSize;
+        this.requiredAdvancement = pRequiredAdvancement;
+        this.forbiddenAdvancement = pForbiddenAdvancement;
     }
 
     /**
@@ -67,8 +74,16 @@ public class DistillationFabricationRecipe implements Recipe<SimpleContainer> {
         return componentMateria;
     }
 
+    public byte getWisdom() {
+        return wisdom;
+    }
+
+    public byte getBatchSize() {
+        return batchSize;
+    }
+
     public boolean getIsDistillOnly() {
-        return distillOnly;
+        return wisdom > 5;
     }
 
     public ItemStack getAlchemyObject() {
@@ -76,6 +91,14 @@ public class DistillationFabricationRecipe implements Recipe<SimpleContainer> {
     }
 
     public float getOutputRate() { return outputRate; }
+
+    public boolean isAdvancementRequired() {
+        return requiredAdvancement != null;
+    }
+
+    public boolean isForbiddenByAdvancement() {
+        return forbiddenAdvancement != null;
+    }
 
     @Override
     public ItemStack assemble(SimpleContainer pContainer, RegistryAccess pRegistryAccess) {
@@ -128,7 +151,7 @@ public class DistillationFabricationRecipe implements Recipe<SimpleContainer> {
         for(DistillationFabricationRecipe acr : allRecipes) {
             if(acr.alchemyObject.getItem() == query.getItem()) {
                 result = acr;
-                if(acr.distillOnly)
+                if(acr.wisdom > 5)
                     return null;
                 break;
             }
@@ -137,12 +160,36 @@ public class DistillationFabricationRecipe implements Recipe<SimpleContainer> {
         return result;
     }
 
+    public static final byte
+        FLAG_CRAFTABLE = 1, FLAG_GATHERABLE = 2, FLAG_FARMABLE = 4, FLAG_RENEWABLE = 8, FLAG_TROPHY = 16, FLAG_RARE = 32;
+    public boolean hasSourceCategory(DistillationSourceCategory pSource) {
+        if(pSource == DistillationSourceCategory.CRAFTABLE) return (categories & FLAG_CRAFTABLE) == FLAG_CRAFTABLE;
+        if(pSource == DistillationSourceCategory.GATHERABLE) return (categories & FLAG_GATHERABLE) == FLAG_GATHERABLE;
+        if(pSource == DistillationSourceCategory.FARMABLE) return (categories & FLAG_FARMABLE) == FLAG_FARMABLE;
+        if(pSource == DistillationSourceCategory.RENEWABLE) return (categories & FLAG_RENEWABLE) == FLAG_RENEWABLE;
+        if(pSource == DistillationSourceCategory.TROPHY) return (categories & FLAG_TROPHY) == FLAG_TROPHY;
+        if(pSource == DistillationSourceCategory.RARE) return (categories & FLAG_RARE) == FLAG_RARE;
+        return false;
+    }
+
+    public NonNullList<DistillationSourceCategory> getSourceCategories() {
+        NonNullList<DistillationSourceCategory> out = NonNullList.create();
+
+        if((categories & FLAG_CRAFTABLE) == FLAG_CRAFTABLE) out.add(DistillationSourceCategory.CRAFTABLE);
+        if((categories & FLAG_GATHERABLE) == FLAG_GATHERABLE) out.add(DistillationSourceCategory.GATHERABLE);
+        if((categories & FLAG_FARMABLE) == FLAG_FARMABLE) out.add(DistillationSourceCategory.FARMABLE);
+        if((categories & FLAG_RENEWABLE) == FLAG_RENEWABLE) out.add(DistillationSourceCategory.RENEWABLE);
+        if((categories & FLAG_TROPHY) == FLAG_TROPHY) out.add(DistillationSourceCategory.TROPHY);
+        if((categories & FLAG_RARE) == FLAG_RARE) out.add(DistillationSourceCategory.RARE);
+
+        return out;
+    }
+
     public static class Type implements RecipeType<DistillationFabricationRecipe> {
         private Type() { }
         public static final Type INSTANCE = new Type();
         public static final String ID = "distillation_fabrication";
     }
-
 
     public static class Serializer implements RecipeSerializer<DistillationFabricationRecipe> {
         public static final Serializer INSTANCE = new Serializer();
@@ -157,8 +204,18 @@ public class DistillationFabricationRecipe implements Recipe<SimpleContainer> {
             if(recipeObject.getItem() == ForgeRegistries.ITEMS.getValue(new ResourceLocation("minecraft:air")))
                 recipeObject = new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation("minecraft:barrier")));
 
-            boolean distillOnly = GsonHelper.getAsBoolean(pSerializedRecipe, "distill_only");
+            byte wisdom = GsonHelper.getAsByte(pSerializedRecipe, "wisdom");
+            byte categories = GsonHelper.getAsByte(pSerializedRecipe, "categories");
+            byte batchSize = GsonHelper.getAsByte(pSerializedRecipe, "batch_size");
             float rate = GsonHelper.getAsFloat(pSerializedRecipe, "output_rate");
+
+            ResourceLocation requiredAdvancementRL = null;
+            if(pSerializedRecipe.has("required_advancement"))
+                requiredAdvancementRL = new ResourceLocation(GsonHelper.getAsString(pSerializedRecipe, "required_advancement"));
+
+            ResourceLocation forbiddenAdvancementRL = null;
+            if(pSerializedRecipe.has("forbidden_advancement"))
+                forbiddenAdvancementRL = new ResourceLocation(GsonHelper.getAsString(pSerializedRecipe, "forbidden_advancement"));
 
             JsonArray components = GsonHelper.getAsJsonArray(pSerializedRecipe, "components");
             NonNullList<ItemStack> extractedIngredients = NonNullList.create();
@@ -179,7 +236,7 @@ public class DistillationFabricationRecipe implements Recipe<SimpleContainer> {
                 extractedIngredients.add(ing);
             });
 
-            return new DistillationFabricationRecipe(pRecipeId, recipeObject, extractedIngredients, distillOnly, rate);
+            return new DistillationFabricationRecipe(pRecipeId, recipeObject, extractedIngredients, wisdom, categories, batchSize, rate, requiredAdvancementRL, forbiddenAdvancementRL);
         }
 
         @Override
@@ -187,7 +244,9 @@ public class DistillationFabricationRecipe implements Recipe<SimpleContainer> {
             CompoundTag nbt = buf.readNbt();
             if(nbt == null) return null;
 
-            boolean readDistillOnly = nbt.getBoolean("distillOnly");
+            byte readWisdom = nbt.getByte("wisdom");
+            byte readCategories = nbt.getByte("categories");
+            byte readBatchSize = nbt.getByte("batchSize");
             float readOutputRate = nbt.getFloat("outputRate");
             ResourceLocation alchemyObjectRL = new ResourceLocation(nbt.getString("alchemyObject"));
             Item alchemyObjectItem = ForgeRegistries.ITEMS.getValue(alchemyObjectRL);
@@ -195,6 +254,13 @@ public class DistillationFabricationRecipe implements Recipe<SimpleContainer> {
             if(alchemyObjectItem != null)
                 alchemyObject = new ItemStack(alchemyObjectItem, 1);
             int componentTotal = nbt.getInt("componentCount");
+
+            ResourceLocation requiredAdvancementRL = null;
+            if(nbt.contains("requiredAdvancement"))
+                requiredAdvancementRL = new ResourceLocation(nbt.getString("requiredAdvancement"));
+            ResourceLocation forbiddenAdvancementRL = null;
+            if(nbt.contains("forbiddenAdvancement"))
+                forbiddenAdvancementRL = new ResourceLocation(nbt.getString("forbiddenAdvancement"));
 
             NonNullList<ItemStack> readComponentMateria = NonNullList.create();
             for(int i=0; i<componentTotal; i++) {
@@ -207,13 +273,19 @@ public class DistillationFabricationRecipe implements Recipe<SimpleContainer> {
                 readComponentMateria.add(componentStack);
             }
 
-            return new DistillationFabricationRecipe(id, alchemyObject, readComponentMateria, readDistillOnly, readOutputRate);
+            return new DistillationFabricationRecipe(id, alchemyObject, readComponentMateria, readWisdom, readCategories, readBatchSize, readOutputRate, requiredAdvancementRL, forbiddenAdvancementRL);
         }
 
         @Override
         public void toNetwork(FriendlyByteBuf buf, DistillationFabricationRecipe recipe) {
             CompoundTag nbt = new CompoundTag();
-            nbt.putBoolean("distillOnly", recipe.distillOnly);
+            nbt.putByte("wisdom", recipe.wisdom);
+            nbt.putByte("categories", recipe.categories);
+            nbt.putByte("batchSize", recipe.batchSize);
+            if(recipe.requiredAdvancement != null)
+                nbt.putString("requiredAdvancement", recipe.requiredAdvancement.toString());
+            if(recipe.forbiddenAdvancement != null)
+                nbt.putString("forbiddenAdvancement", recipe.forbiddenAdvancement.toString());
             nbt.putFloat("outputRate", recipe.outputRate);
             nbt.putString("alchemyObject", ForgeRegistries.ITEMS.getKey(recipe.getAlchemyObject().getItem()).toString());
             nbt.putInt("componentCount", recipe.componentMateria.size());
