a78db0efef588999291b94743dca087880b1fd2b
AranaiRa <aranaiseraph@gmail.com>
2025-06-14 06:03:07 -0400

    Acid Basin has proper render bounds

--- src/main/java/com/aranaira/magichem/block/entity/AcidBasinBlockEntity.java
+++ src/main/java/com/aranaira/magichem/block/entity/AcidBasinBlockEntity.java
@@ -25,6 +25,7 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.phys.AABB;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
@@ -426,5 +427,10 @@ public class AcidBasinBlockEntity extends BlockEntity implements IFluidHandler, 
         stack.setTag(nbt);
 
         Containers.dropItemStack(level, worldPosition.getX(), worldPosition.getY(), worldPosition.getZ(), stack);
+    }
+
+    @Override
+    public AABB getRenderBoundingBox() {
+        return new AABB(getBlockPos().offset(-1, 0, -1), getBlockPos().offset(1,1,1));
     }
 }
