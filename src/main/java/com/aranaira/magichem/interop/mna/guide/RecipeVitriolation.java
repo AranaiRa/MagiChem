e648e808d5064781aabdec166b659985e8efaa05
AranaiRa <aranaiseraph@gmail.com>
2025-06-22 04:21:47 -0400

    Rendering for Codex Vitriolation recipes

--- src/main/java/com/aranaira/magichem/interop/mna/guide/RecipeVitriolation.java
+++ src/main/java/com/aranaira/magichem/interop/mna/guide/RecipeVitriolation.java
@@ -22,24 +22,56 @@ public class RecipeVitriolation extends RecipeRendererBase {
 
     protected void drawForeground(GuiGraphics pGuiGraphics, int pX, int pY, int pMouseX, int pMouseY, float pPartialTicks) {
         if(this.recipe != null) {
-            this.renderItemStack(pGuiGraphics, this.recipe.getInputItem(),
-                    (int)((float)this.getX() / this.scale + 43f),
-                    (int)((float)this.getY() / this.scale + 111.0f)
+            this.renderItemStack(pGuiGraphics, new ItemStack(this.recipe.getInputItem().getItem()),
+                    (int)((float)this.getX() / this.scale + 87.5f),
+                    (int)((float)this.getY() / this.scale + 50.0f)
             );
-            this.renderItemStack(pGuiGraphics, new ItemStack(VitriolationRecipe.getAllFluidsOfAcidStrength(this.recipe.getMinimumAcidStrength()).get(0).getBucket(), this.recipe.getBaseFluidConsumed()),
-                    (int)((float)this.getX() / this.scale + 43f),
-                    (int)((float)this.getY() / this.scale + 111.0f)
-            );
+            if(this.recipe.getInputItem().getCount() > 1) {
+                pGuiGraphics.drawString(Minecraft.getInstance().font,
+                        "x"+this.recipe.getInputItem().getCount(),
+                        (int) ((float) this.getX() / this.scale + 82f),
+                        (int) ((float) this.getY() / this.scale + 76.0f),
+                        0xff000000, false
+                );
+            }
+
+            if(this.recipe.hasInputFluidOverride()) {
+                this.renderItemStack(pGuiGraphics, new ItemStack(this.recipe.getInputFluidOverride().getBucket()),
+                        (int) ((float) this.getX() / this.scale + 115.5f),
+                        (int) ((float) this.getY() / this.scale + 50.0f)
+                );
+                pGuiGraphics.drawString(Minecraft.getInstance().font,
+                        this.recipe.getBaseFluidConsumed()+" mB",
+                        (int) ((float) this.getX() / this.scale + 122f),
+                        (int) ((float) this.getY() / this.scale + 76.0f),
+                        0xff000000, false
+                );
+            }
+            else if(this.recipe.getMinimumAcidStrength() > 0) {
+                this.renderItemStack(pGuiGraphics, new ItemStack(VitriolationRecipe.getAllFluidsOfAcidStrength(this.recipe.getMinimumAcidStrength()).get(0).getBucket()),
+                        (int) ((float) this.getX() / this.scale + 115f),
+                        (int) ((float) this.getY() / this.scale + 50.0f)
+                );
+            }
+
             if(this.recipe.hasResultFluid()) {
-                this.renderItemStack(pGuiGraphics, new ItemStack(this.recipe.getResultFluid().getFluid().getBucket(), this.recipe.getResultFluid().getAmount()),
-                        (int) ((float) this.getX() / this.scale + 99f),
-                        (int) ((float) this.getY() / this.scale + 111.0f)
+                this.renderItemStack(pGuiGraphics, new ItemStack(this.recipe.getResultFluid().getFluid().getBucket()),
+                        (int) ((float) this.getX() / this.scale + 160f),
+                        (int) ((float) this.getY() / this.scale + 175.0f)
+                );
+                String out = this.recipe.getResultFluid().getAmount()+" mB";
+                int width = Minecraft.getInstance().font.width(out);
+                pGuiGraphics.drawString(Minecraft.getInstance().font,
+                        out,
+                        (int) ((float) this.getX() / this.scale + 168f - (width * 0.5f)),
+                        (int) ((float) this.getY() / this.scale + 210.0f),
+                        0xff000000, false
                 );
             }
             if(this.recipe.hasResultItem()) {
                 this.renderItemStack(pGuiGraphics, this.recipe.getResultItem(),
-                        (int) ((float) this.getX() / this.scale + 160f),
-                        (int) ((float) this.getY() / this.scale + 111.0f)
+                        (int) ((float) this.getX() / this.scale + 43f),
+                        (int) ((float) this.getY() / this.scale + 175.0f)
                 );
             }
         }
