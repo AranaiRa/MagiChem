7f04ae979a133ff76d591e96acd0472e48cd26d2
YukkuriC <799433638@qq.com>
2025-08-28 23:16:08 +0800

    materia filter uses lang instead of raw component

--- src/main/java/com/aranaira/magichem/gui/MateriaManifestScreen.java
+++ src/main/java/com/aranaira/magichem/gui/MateriaManifestScreen.java
@@ -18,6 +18,7 @@ import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.locale.Language;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
@@ -193,9 +194,7 @@ public class MateriaManifestScreen extends AbstractContainerScreen<MateriaManife
         orderedMateriaStorageFiltered.clear();
         for (Pair<MateriaItem, BlockEntity> pair : orderedMateriaStorage) {
             MateriaItem mi = pair.getFirst();
-            String name = mi instanceof EssentiaItem ?
-                    Component.translatable("item.magichem.essentia_" + mi.getMateriaName()).toString() :
-                    Component.translatable("item.magichem.admixture_" + mi.getMateriaName()).toString();
+            String name = Language.getInstance().getOrDefault((mi instanceof EssentiaItem ? "item.magichem.essentia_" : "item.magichem.admixture_") + mi.getMateriaName()).toLowerCase(Locale.ROOT);
 
             if(filter.equals("") || name.contains(filter)) {
                 orderedMateriaStorageFiltered.add(pair);
