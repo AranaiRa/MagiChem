dc496fd25bb3901c590dc303e960ef8566f52664
AranaiRa <aranaiseraph@gmail.com>
2025-10-22 12:37:57 -0400

    JEI panel for Illumination now shows lens and craft time differences

--- src/main/java/com/aranaira/magichem/interop/jei/IlluminationRecipeCategory.java
+++ src/main/java/com/aranaira/magichem/interop/jei/IlluminationRecipeCategory.java
@@ -1,6 +1,7 @@
 package com.aranaira.magichem.interop.jei;
 
 import com.aranaira.magichem.MagiChemMod;
+import com.aranaira.magichem.config.ServerConfig;
 import com.aranaira.magichem.foundation.enums.LuminType;
 import com.aranaira.magichem.interop.JEIPlugin;
 import com.aranaira.magichem.recipe.IlluminationRecipe;
@@ -21,6 +22,7 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+import org.apache.logging.log4j.core.jmx.Server;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,9 +35,22 @@ public class IlluminationRecipeCategory implements IRecipeCategory<IlluminationR
     private final IDrawable background;
     private final IDrawable icon;
 
+    private static ItemStack
+        LENS_GLASS = null,
+        LENS_CLOISTER_SOLAR = null, LENS_CLOISTER_LUNAR = null, LENS_CLOISTER_SIDEREAL = null,
+        LENS_FARSIGHT_SOLAR = null, LENS_FARSIGHT_LUNAR = null, LENS_FARSIGHT_SIDEREAL = null;
+
     public IlluminationRecipeCategory(IGuiHelper helper) {
         this.background = helper.createDrawable(TEXTURE, 96, 110, 96, 110);
         this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ItemRegistry.DUMMY_ILLUMINATION.get()));
+
+        if(LENS_GLASS == null) LENS_GLASS = new ItemStack(ItemRegistry.GLASS_LENS.get());
+        if(LENS_CLOISTER_SOLAR == null) LENS_CLOISTER_SOLAR = new ItemStack(ItemRegistry.SOLAR_CLOISTER_LENS.get());
+        if(LENS_CLOISTER_LUNAR == null) LENS_CLOISTER_LUNAR = new ItemStack(ItemRegistry.LUNAR_CLOISTER_LENS.get());
+        if(LENS_CLOISTER_SIDEREAL == null) LENS_CLOISTER_SIDEREAL = new ItemStack(ItemRegistry.SIDEREAL_CLOISTER_LENS.get());
+        if(LENS_FARSIGHT_SOLAR == null) LENS_FARSIGHT_SOLAR = new ItemStack(ItemRegistry.SOLAR_FARSIGHT_LENS.get());
+        if(LENS_FARSIGHT_LUNAR == null) LENS_FARSIGHT_LUNAR = new ItemStack(ItemRegistry.LUNAR_FARSIGHT_LENS.get());
+        if(LENS_FARSIGHT_SIDEREAL == null) LENS_FARSIGHT_SIDEREAL = new ItemStack(ItemRegistry.SIDEREAL_FARSIGHT_LENS.get());
     }
 
     @Override
@@ -67,10 +82,21 @@ public class IlluminationRecipeCategory implements IRecipeCategory<IlluminationR
     public void draw(IlluminationRecipe recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics gui, double mouseX, double mouseY) {
         IRecipeCategory.super.draw(recipe, recipeSlotsView, gui, mouseX, mouseY);
 
-        gui.blit(TEXTURE, 13, 3, 192, 110 + (recipe.getLuminType().ordinal() - 1) * 14,14, 14);
+        gui.blit(TEXTURE, 13, 5, 192, 110 + (recipe.getLuminType().ordinal() - 1) * 14,14, 14);
         final Font font = Minecraft.getInstance().font;
 
+        long loop = Minecraft.getInstance().level.getGameTime() % 180;
         int opTime = recipe.getCraftTime();
+
+        if(loop >= 120) {
+            gui.renderItem(recipe.getLuminType() == LuminType.SOLAR ? LENS_FARSIGHT_SOLAR : recipe.getLuminType() == LuminType.LUNAR ? LENS_FARSIGHT_LUNAR : LENS_FARSIGHT_SIDEREAL, 70, 4);
+            opTime = Math.round((float)opTime / ((float)ServerConfig.astralObserverLuminGainFarsight / (float)ServerConfig.astralObserverLuminGainStandard));
+        } else if(loop >= 60) {
+            gui.renderItem(recipe.getLuminType() == LuminType.SOLAR ? LENS_CLOISTER_SOLAR : recipe.getLuminType() == LuminType.LUNAR ? LENS_CLOISTER_LUNAR : LENS_CLOISTER_SIDEREAL, 70, 4);
+            opTime = Math.round((float)opTime / ((float)ServerConfig.astralObserverLuminGainCloister / (float)ServerConfig.astralObserverLuminGainStandard));
+        } else {
+            gui.renderItem(LENS_GLASS, 70, 4);
+        }
         int hourWhole = opTime / 60;
         int minWhole = opTime % 60;
 
@@ -84,7 +110,7 @@ public class IlluminationRecipeCategory implements IRecipeCategory<IlluminationR
         if(minWhole > 0) {
             time += minWhole + "m";
         }
-        gui.drawString(font, time, 21 - font.width(time) / 2, 21, 0xff000000, false);
+        gui.drawString(font, time, 79 - font.width(time) / 2, 24, 0xff000000, false);
     }
 
     @Override
@@ -93,7 +119,7 @@ public class IlluminationRecipeCategory implements IRecipeCategory<IlluminationR
         ArrayList<Component> out = new ArrayList<>();
 
         boolean xCoord = mouseX >= 13 && mouseX <= 27;
-        boolean yCoord = mouseY >= 3  && mouseY <= 17;
+        boolean yCoord = mouseY >= 5  && mouseY <= 19;
         if(in.size() > 0) {
             for (Object o : in.stream().toArray()) {
                 if (o instanceof Component c) {
