3959e90d5029d0c0145d6b65a1eae093ac1cc41b
AranaiRa <aranaiseraph@gmail.com>
2025-11-03 10:01:10 -0500

    Navigator speed settings are now respected in Separation devices

--- src/main/java/com/aranaira/magichem/block/entity/ext/AbstractSeparationBlockEntity.java
+++ src/main/java/com/aranaira/magichem/block/entity/ext/AbstractSeparationBlockEntity.java
@@ -172,7 +172,7 @@ public abstract class AbstractSeparationBlockEntity extends AbstractBlockEntityW
                             for (MateriaItem mi : provisioningNeeds.keySet()) {
                                 int requested = provisioningNeeds.get(mi);
                                 int inStorage = multi.getCurrentStock(mi);
-                                boolean instant = ender.getPowerLevel() == 3;
+                                boolean instant = ender.getPowerLevel() == 2;
 
                                 int actualDrain = Math.min(requested, inStorage);
                                 if (actualDrain > 0) {
