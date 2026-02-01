d7ca8c835872a0c683aa098aa3d043238a237ec6
YukkuriC <799433638@qq.com>
2026-02-01 16:24:18 +0800

    bugfix: labyrinth data desync

--- src/main/java/com/aranaira/magichem/gui/MirrorLabyrinthScreen.java
+++ src/main/java/com/aranaira/magichem/gui/MirrorLabyrinthScreen.java
@@ -217,6 +217,10 @@ public class MirrorLabyrinthScreen extends AbstractContainerScreen<MirrorLabyrin
                 orderedMateriaStorageFiltered.add(pair);
             }
         }
+
+        pageCount = (int)Math.ceil((float)orderedMateriaStorageFiltered.size() / 16f);
+        if(pageCount <= 0)
+            pageCount = 1;
     }
 
     private MateriaItem getMateriaTypeFromButtonID(int pButtonID) {
@@ -281,10 +285,6 @@ public class MirrorLabyrinthScreen extends AbstractContainerScreen<MirrorLabyrin
         for(MateriaItem mi : menu.blockEntity.getMateriaTypesSorted()) {
             orderedMateriaStorage.add(new Pair<>(mi, menu.blockEntity.getCurrentStock(mi)));
         }
-
-        pageCount = (int)Math.ceil((float)orderedMateriaStorageFiltered.size() / 16f);
-        if(pageCount <= 0)
-            pageCount = 1;
 
         menu.blockEntity.needsGuiStorageUpdate = false;
     }
