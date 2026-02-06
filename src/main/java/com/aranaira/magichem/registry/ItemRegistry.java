a8daa40c5249d792408dee88f6767ca9675f9e78
AranaiRa <aranaiseraph@gmail.com>
2026-02-06 00:11:06 -0500

    MnA bosses drop boss trophies

--- src/main/java/com/aranaira/magichem/registry/ItemRegistry.java
+++ src/main/java/com/aranaira/magichem/registry/ItemRegistry.java
@@ -506,20 +506,20 @@ public class ItemRegistry {
             () -> new TooltipLoreItem(new Item.Properties().stacksTo(1))
     );
 
-    public static final RegistryObject<TooltipLoreItem> BOSS_TROPHY_COUNCIL = ITEMS.register("boss_trophy_council",
-            () -> new TooltipLoreItem(new Item.Properties().stacksTo(1))
+    public static final RegistryObject<IndestructibleItem> BOSS_TROPHY_COUNCIL = ITEMS.register("boss_trophy_council",
+            () -> new IndestructibleItem(new Item.Properties().stacksTo(1))
     );
 
-    public static final RegistryObject<TooltipLoreItem> BOSS_TROPHY_FEY = ITEMS.register("boss_trophy_fey",
-            () -> new TooltipLoreItem(new Item.Properties().stacksTo(1))
+    public static final RegistryObject<IndestructibleItem> BOSS_TROPHY_FEY = ITEMS.register("boss_trophy_fey",
+            () -> new IndestructibleItem(new Item.Properties().stacksTo(1))
     );
 
-    public static final RegistryObject<TooltipLoreItem> BOSS_TROPHY_DEMONS = ITEMS.register("boss_trophy_demons",
-            () -> new TooltipLoreItem(new Item.Properties().stacksTo(1))
+    public static final RegistryObject<IndestructibleItem> BOSS_TROPHY_DEMONS = ITEMS.register("boss_trophy_demons",
+            () -> new IndestructibleItem(new Item.Properties().stacksTo(1))
     );
 
-    public static final RegistryObject<TooltipLoreItem> BOSS_TROPHY_UNDEAD = ITEMS.register("boss_trophy_undead",
-            () -> new TooltipLoreItem(new Item.Properties().stacksTo(1))
+    public static final RegistryObject<IndestructibleItem> BOSS_TROPHY_UNDEAD = ITEMS.register("boss_trophy_undead",
+            () -> new IndestructibleItem(new Item.Properties().stacksTo(1))
     );
 
     public static final RegistryObject<TooltipLoreItem> CROWN_OF_GLORY = ITEMS.register("crown_of_glory",
