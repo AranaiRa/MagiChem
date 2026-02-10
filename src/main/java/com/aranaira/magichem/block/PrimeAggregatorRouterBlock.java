96a06a2b4d3bbb4e38066c6939f2d442dce6471a
AranaiRa <aranaiseraph@gmail.com>
2026-02-10 02:43:55 -0500

    Fixed pick block on the Aggregator

--- src/main/java/com/aranaira/magichem/block/PrimeAggregatorRouterBlock.java
+++ src/main/java/com/aranaira/magichem/block/PrimeAggregatorRouterBlock.java
@@ -12,6 +12,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
@@ -55,6 +56,11 @@ public class PrimeAggregatorRouterBlock extends BaseEntityBlock implements INoCr
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
         pBuilder.add(ROUTER_TYPE_PRIME_AGGREGATOR, BlockStateProperties.HORIZONTAL_FACING);
+    }
+
+    @Override
+    public ItemStack getCloneItemStack(BlockGetter pLevel, BlockPos pPos, BlockState pState) {
+        return new ItemStack(BlockRegistry.PRIME_AGGREGATOR.get());
     }
 
     @Override
