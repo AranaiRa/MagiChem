40ceab4804cb713fb77536486f6b028f7e00e5ea
AranaiRa <aranaiseraph@gmail.com>
2026-03-15 06:01:58 -0400

    Missed Selenolabe in the updaters

--- src/main/java/com/aranaira/magichem/block/entity/SelenolabeBlockEntity.java
+++ src/main/java/com/aranaira/magichem/block/entity/SelenolabeBlockEntity.java
@@ -1,21 +1,30 @@
 package com.aranaira.magichem.block.entity;
 
+import com.aranaira.magichem.block.SelenolabeBlock;
 import com.aranaira.magichem.registry.BlockEntitiesRegistry;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class SelenolabeBlockEntity extends BlockEntity {
+    private int signalLastTick = 0;
+
     public SelenolabeBlockEntity(BlockPos pPos, BlockState pBlockState) {
         super(BlockEntitiesRegistry.SELENOLABE_BE.get(), pPos, pBlockState);
     }
 
     public static <E extends BlockEntity> void tick(Level pLevel, BlockPos pPos, BlockState pState, E e) {
         if(e instanceof SelenolabeBlockEntity sbe) {
-            if(pLevel.getGameTime() % 1200 == 0) {
-                pLevel.updateNeighborsAt(pPos, sbe.getBlockState().getBlock());
+            if(pLevel.getGameTime() % 300 == 0) {
+                int signalThisTick = sbe.getBlockState().getBlock().getSignal(pState, pLevel, pPos, Direction.UP);
+                if(sbe.signalLastTick != signalThisTick) {
+                    sbe.setChanged();
+                    pLevel.updateNeighborsAt(pPos, sbe.getBlockState().getBlock());
+                }
+                sbe.signalLastTick = signalThisTick;
             }
         }
     }
