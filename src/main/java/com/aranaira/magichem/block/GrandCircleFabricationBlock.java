dc87025e0c574d57a3edfdc7bbf1b6a961067e7a
AranaiRa <aranaiseraph@gmail.com>
2026-03-20 21:09:17 -0400

    Fixed the empty fluid handler itemstack preventing GUI from opening bug on the (Grand) Circle of Fabrication

--- src/main/java/com/aranaira/magichem/block/GrandCircleFabricationBlock.java
+++ src/main/java/com/aranaira/magichem/block/GrandCircleFabricationBlock.java
@@ -110,7 +110,7 @@ public class GrandCircleFabricationBlock extends BaseEntityBlock {
             else if(be instanceof GrandCircleFabricationRouterBlockEntity router)
                 capabilityQuery = router.getCapability(ForgeCapabilities.FLUID_HANDLER);
 
-            if(itemCapabilityQuery.isPresent() && capabilityQuery != null) {
+            if(!itemInHand.isEmpty() && itemCapabilityQuery.isPresent() && capabilityQuery != null) {
                 final IFluidHandler fluidHandler = capabilityQuery.resolve().get();
                 final IFluidHandlerItem iCap = itemCapabilityQuery.resolve().get();
                 final FluidStack fluidInItem = iCap.getFluidInTank(0);
