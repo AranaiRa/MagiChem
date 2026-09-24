85409fa002535b8488a99c9a854b2880b4af6e45
AranaiRa <aranaiseraph@gmail.com>
2026-09-24 16:54:06 -0400

    Added Camouflage to the default Lesser Magnitude Wisdom blacklist

--- src/main/java/com/aranaira/magichem/config/ServerConfig.java
+++ src/main/java/com/aranaira/magichem/config/ServerConfig.java
@@ -527,6 +527,7 @@ public class ServerConfig
     private static final List<String> WISDOM_BLACKLIST_SPEED_DEFAULTS = new ArrayList<>();
     static {
         WISDOM_BLACKLIST_LESSER_MAGNITUDE_DEFAULTS.add("mna:components/telekinesis");
+        WISDOM_BLACKLIST_LESSER_MAGNITUDE_DEFAULTS.add("mna:components/camouflage");
         WISDOM_BLACKLIST_MAGNITUDE_DEFAULTS.add("mna:shapes/sigil");
         WISDOM_BLACKLIST_MAGNITUDE_DEFAULTS.add("mna:components/shield");
         WISDOM_BLACKLIST_MAGNITUDE_DEFAULTS.add("mna:components/simulacrum");
